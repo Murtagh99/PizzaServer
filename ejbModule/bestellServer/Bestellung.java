ef28470794f260803a2ef8049d7ecd20506d9f33
Pascal Zöller <pascal.zoeller@itc-studenten.de>
2021-05-27 13:30:53 +0200

    Bestellung angepasst

--- ejbModule/bestellServer/Bestellung.java
+++ ejbModule/bestellServer/Bestellung.java
@@ -1,20 +1,42 @@
 package bestellServer;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-/**
- * Session Bean implementation class Bestellung
- */
+
 @Stateful
 @LocalBean
-public class Bestellung implements BestellungRemote, BestellungLocal {
+@Entity
+public class Bestellung implements BestellungRemote, BestellungLocal, Serializable {
 
-    /**
-     * Default constructor. 
-     */
+	@Id
+	@GeneratedValue
+	private int id;
+	private Bestellstatus bestellstatus;
+	private String zahlungsart;
+	private ArrayList<Object> gerichte;
+	private Object kunde;
+	private boolean lieferung;
+	private LocalDate bestellzeit;
+	
     public Bestellung() {
         // TODO Auto-generated constructor stub
     }
 
+}
+
+enum Bestellstatus {
+	AENDERBAR,
+	WARTEND,
+	IN_BEARBEITUNG,
+	IN_AUSLIEFERUNG,
+	ABHOLBEREIT,
+	ABGESCHLOSSEN,
 }
